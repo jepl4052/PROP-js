82b450c8827a84d8a59b2187f7fe30127bc89d16
marcuspousette <marcuspousette@gmail.com>
2018-11-28 12:52:05 +0100

    push

--- mi_js/part_one.js
+++ mi_js/part_one.js
@@ -4,40 +4,65 @@
 * erho
 */
 
+
 var myObject = {
 
   "create": function(prototypeList){
     let obj = Object.create(this);
+    if(prototypeList != null && !Array.isArray(prototypeList)){
+      obj = null;
+      console.error("Bad params");
+    }
     if(prototypeList != null){
-      obj.prototypes = prototypeList;
+      //filter duplicate args
+      prototypeList = prototypeList.filter((item, pos, self) => self.indexOf(item) == pos);
+      obj.prototypeList = prototypeList;
     }
     return obj;
   },
 
-  'call': function (funcName, parameters) {
+  "call": function (funcName, parameters) {
     	var visited = [];
-    	return this.findFunc(funcName, parameters, visited);
+    	return this.depthSearch(funcName, parameters, visited);
     },
-    'findFunc': function (funcName, parameters, visited) {
-		visited.push(this);
-		if(this.hasOwnProperty(funcName)) {
-			return this[funcName](parameters);
-		}
-		var result;
-		this.prototypes.forEach(
-			function(obj) {
-				if(visited.indexOf(obj) == -1) {
-					var call = obj.findFunc(funcName, parameters, visited)
+
+    "depthSearch": function (funcName, parameters, visited) {
+  		visited.push(this);
+  		if(this.hasOwnProperty(funcName)) {
+  			return this[funcName](parameters);
+  		}
+
+      var result;
+      this.prototypeList.forEach( obj => {
+        if(visited.indexOf(obj) === -1) {
+          //obj not found in visited[]
+          var call = obj.depthSearch(funcName, parameters, visited)
 					if(call != undefined && result === undefined) {
 						result = call;
 						return;
 					}
-				}
-				else
-					console.log("WARNING: Circular inheritance detected!");
-			}
-		);
-		return result;
+        }else{
+          console.log("Circular inheritance");
+        }
+      });
+      return result;
+    },
+
+    addPrototype:function(obj){
+      console.log(this);
+      obj.prototypeList.forEach((proto) => {
+        if(Object.is(proto, this) || Object.is(obj, this)){
+          console.log("Circular");
+          return;
+        }else{
+          if(!this.prototypeList){
+            var prototypeList = [];
+            this.prototypeList.push(obj);
+          }else{
+            this.prototypeList.push(obj);
+          }
+        }
+      });
     },
 
 }
@@ -47,14 +72,20 @@ try{
   /*
   * Test 1
   */
-  // var obj0 = myObject.create(null);
-  // obj0.func = function(arg) { return "func0: " + arg; };
-  // var obj1 = myObject.create([obj0]);
-  // var obj2 = myObject.create([]);
-  // obj2.func = function(arg) { return "func2: " + arg; };
-  // var obj3 = myObject.create([obj1, obj2]);
-  // var result = obj3.call("func", ["hello"]) ;
-  // console.log("should print 'func0: hello' ->", result);
+  var obj0 = myObject.create(null);
+  obj0.func = function(arg) { return "func0: " + arg; };
+  var obj1 = myObject.create([obj0]);
+  var obj2 = myObject.create([]);
+  obj2.func = function(arg) { return "func2: " + arg; };
+  var obj3 = myObject.create([obj1, obj2]);
+  var result = obj3.call("func", ["hello"]) ;
+  console.log("should print 'func0: hello' ->", result);
+  obj0.addPrototype(obj1);
+  console.log(obj0);
+  console.log(obj1);
+  obj1.addPrototype(obj3);
+  console.log(obj1);
+
 
 /*
 * Test 2
@@ -67,13 +98,13 @@ try{
 /*
 * Test 3
 */
-obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
-obj1 = myObject.create([obj0]);
-obj2 = myObject.create([]);
-obj3 = myObject.create([obj2, obj1]);
-result = obj3.call("func", ["hello"]);
-console.log("should print 'func0: hello' ->", result);
+// obj0 = myObject.create(null);
+// obj0.func = function(arg) { return "func0: " + arg; };
+// obj1 = myObject.create([obj0]);
+// obj2 = myObject.create([]);
+// obj3 = myObject.create([obj2, obj1]);
+// result = obj3.call("func", ["hello"]);
+// console.log("should print 'func0: hello' ->", result);
 
 } catch(err){
   console.log(err);
